7f2e0e226b4da277d3799e97004c3852b58624c0
Omkar Chandorkar <gotenksIN@aospa.co>
2025-10-27 18:05:23 +0530

    phoenix: Sync extract-utils with template

--- extract-files.sh
+++ extract-files.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #
-# Copyright (C) 2018 The LineageOS Project
-# Copyright (C) 2019-2021 Paranoid Android
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2022 Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -25,28 +15,42 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="$ROOT"/tools/extract-utils/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -n | --no-cleanup )     CLEAN_VENDOR=false
-        ;;
-        -s | --section )        shift
-        SECTION=$1
-        CLEAN_VENDOR=false
-        ;;
-        * )                     SRC=$1
-        ;;
+ONLY_FIRMWARE=
+KANG=
+SECTION=
+CARRIER_SKIP_FILES=()
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        --only-firmware)
+            ONLY_FIRMWARE=true
+            ;;
+        -n | --no-cleanup)
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang)
+            KANG="--kang"
+            ;;
+        -s | --section)
+            SECTION="${2}"
+            shift
+            CLEAN_VENDOR=false
+            ;;
+        *)
+            SRC="${1}"
+            ;;
     esac
     shift
 done
@@ -58,22 +62,30 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/lib64/libgoodixhwfingerprint.so )
+        [ "$2" = "" ] && return 0
         "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
         vendor/lib64/hw/camera.qcom.so | vendor/lib64/libmegface.so | vendor/lib64/libFaceDetectpp-0.5.2.so)
+         [ "$2" = "" ] && return 0
             sed -i "s|libmegface.so|libfacedet.so|g" "${2}"
             sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
             sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
             ;;
         vendor/lib64/camera/components/com.qti.node.watermark.so)
+         [ "$2" = "" ] && return 0
             ${PATCHELF} --add-needed "libpiex_shim.so" "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
 }
 
-# Initialize the helper for common device
-setup_vendor "$DEVICE" "$VENDOR" "$ROOT" false "$CLEAN_VENDOR"
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
-"$MY_DIR"/setup-makefiles.sh
+"${MY_DIR}/setup-makefiles.sh"
